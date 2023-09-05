d0e95e6da28c84bb3d19f09886641f49005b834e
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-09-05 16:28:20 +0200

    Skipping test modules

--- src/commands/deploy.sh
+++ src/commands/deploy.sh
@@ -1,5 +1,5 @@
 if [[ "$PWD" = */portal-test ]]; then
     cliferay ant deploy install-portal-snapshot && cp ../../bundles/osgi/test/com.liferay.portal.test.jar ../../bundles/osgi/modules
-else
+elif ! [[ "$PWD" = */*-test ]]; then
     cliferay gw -a depl
 fi
