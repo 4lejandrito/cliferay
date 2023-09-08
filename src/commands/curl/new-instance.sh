7940c5fff303d1e928a704493c5c8972e7d312ed
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-09-08 13:31:59 +0200

    Adding host parameter

--- src/commands/curl/new-instance.sh
+++ src/commands/curl/new-instance.sh
@@ -1,10 +1,11 @@
+HOST=${args["host"]}
 curl --no-progress-meter -X 'POST' \
   'http://localhost:8080/o/headless-portal-instances/v1.0/portal-instances' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "domain": "able.com",
-  "portalInstanceId": "able.com",
-  "virtualHost": "www.able.com"
+  "domain": "'$HOST'",
+  "portalInstanceId": "'$HOST'",
+  "virtualHost": "'$HOST'"
 }' \
 -u 'test@liferay.com:test' | jq
