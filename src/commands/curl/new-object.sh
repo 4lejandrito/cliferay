e9c5c0a4cb6854a791009d2fc0a16b5052701d26
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-09-13 10:51:01 +0200

    Adding new-object curl

---
+++ src/commands/curl/new-object.sh
@@ -0,0 +1,57 @@
+JSON=$(
+  curl --no-progress-meter -X "PUT" "http://localhost:8080/o/object-admin/v1.0/object-definitions/by-external-reference-code/University" \
+     -H 'Content-Type: application/json' \
+     -u 'test@liferay.com:test' \
+     -d $'{
+        "active": true,
+        "status": {
+          "code": 0
+        },
+        "objectFields": [
+          {
+            "DBType": "String",
+            "indexed": true,
+            "indexedLanguageId": "",
+            "externalReferenceCode": "university-name",
+            "listTypeDefinitionId": 0,
+            "label": {
+              "en_US": "University name"
+            },
+            "type": "String",
+            "required": true,
+            "system": false,
+            "name": "universityName",
+            "indexedAsKeyword": false
+          }
+        ],
+        "pluralLabel": {
+          "en-US": "Universities"
+        },
+        "portlet": true,
+        "scope": "company",
+        "label": {
+          "en-US": "University"
+        },
+        "externalReferenceCode": "University",
+        "name": "University"
+      }'
+)
+
+echo $JSON | jq
+
+if [[ $(echo $JSON | jq '.status.code') != 0 ]]; then
+  curl --no-progress-meter -X "POST" "http://localhost:8080/o/object-admin/v1.0/object-definitions/$(echo $JSON | jq '.id')/publish" \
+     -u 'test@liferay.com:test' | jq
+fi
+
+curl --no-progress-meter -X "POST" "http://localhost:8080/o/c/universities/batch" \
+     -H 'Content-Type: application/json' \
+     -u 'test@liferay.com:test' \
+     -d $'[
+  {
+    "universityName": "Oxford"
+  },
+  {
+    "universityName": "Cambridge"
+  }
+]' | jq
