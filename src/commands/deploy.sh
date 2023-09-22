0b8370d6c43abbd6b4cc39a8b2100458d58183d0
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-09-22 07:53:55 +0200

    Deploying portal-kernel

--- src/commands/deploy.sh
+++ src/commands/deploy.sh
@@ -1,5 +1,7 @@
 if [[ "$PWD" = */portal-test ]]; then
     cliferay ant deploy install-portal-snapshot && cp ../../bundles/osgi/test/com.liferay.portal.test.jar ../../bundles/osgi/modules
+elif [[ "$PWD" = */portal-kernel ]]; then
+    cliferay ant deploy install-portal-snapshot
 elif ! [[ "$PWD" = */*-test ]]; then
     cliferay gw -a depl
 fi
