64b47d60ae296769e85f27756752caf43c6e84c2
Alejandro Tardín <alejandro.tardin@liferay.com>
2024-01-17 13:44:21 +0100

    Deploying test-util modules too

--- src/commands/deploy.sh
+++ src/commands/deploy.sh
@@ -2,6 +2,10 @@ if [[ "$PWD" = */portal-test ]]; then
     cliferay ant deploy install-portal-snapshot && cp ../../bundles/osgi/test/com.liferay.portal.test.jar ../../bundles/osgi/modules
 elif [[ "$PWD" = */portal-kernel ]]; then
     cliferay ant deploy install-portal-snapshot
+elif [[ "$PWD" = *test-util ]]; then
+    cliferay gw -a depl
+    JAR=$( echo "$(realpath $PWD --relative-to $(cliferay folder))" | sed 's#modules/apps/[^/]*/\([^/]*\)-test-util#\1#' | sed 's/-/./g' | sed 's/\(.*\)/com.liferay.\1.test.util.jar/')
+    cp "$(cliferay folder)/../bundles/osgi/test/$JAR" "$(cliferay folder)/../bundles/osgi/modules/"
 elif ! [[ "$PWD" = */*-test ]]; then
     cliferay gw -a depl
 fi
