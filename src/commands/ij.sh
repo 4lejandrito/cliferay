af8abac7291710963ceae0c5bbb0a50b8176155e
Alejandro Tardín <alejandro.tardin@liferay.com>
2024-03-05 10:01:14 +0100

    Automatically updating ij

--- src/commands/ij.sh
+++ src/commands/ij.sh
@@ -2,4 +2,5 @@ cd $(cliferay folder)
 if [ ! -d "../liferay-intellij" ]; then
     gh repo clone holatuwol/liferay-intellij "../liferay-intellij"
 fi
+(cd ../liferay-intellij; git pull)
 ../liferay-intellij/intellij
