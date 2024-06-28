451918a3e6bfbb60e42f2ece5515fd6ad115a705
Alejandro Tardín <alejandro.tardin@liferay.com>
2024-06-28 12:41:11 +0200

    Q2

--- src/commands/stats/tickets.sh
+++ src/commands/stats/tickets.sh
@@ -1,3 +1,3 @@
 cd $(cliferay folder)
 emails=${args[emails]:-}
-git log --since="2024-01-01" --until="2024-04-01" --pretty=format:'%h %s' $(echo ${emails//\"/} | sed 's/[^ ]* */--author=& /g') | cliferay tickets
+git log --since="2024-04-01" --until="2024-07-01" --pretty=format:'%h %s' $(echo ${emails//\"/} | sed 's/[^ ]* */--author=& /g') | cliferay tickets
