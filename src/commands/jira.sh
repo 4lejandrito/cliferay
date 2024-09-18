aa1d99978a900372bb935f726402caf9fa2cf299
Jorge González <jorge.gonzalez@liferay.com>
2024-09-18 18:50:22 +0200

    feat(jira) Search in the current branch name for LP[A-Z]-[0-9]

--- src/commands/jira.sh
+++ src/commands/jira.sh
@@ -1,1 +1,12 @@
-open "https://liferay.atlassian.net/browse/${args["ticket"]}"
+TICKET=${args["ticket"]:-$(git rev-parse --abbrev-ref HEAD | grep -oE 'LP[A-Z]-[0-9]+' || true)}
+
+# var validation
+if [[ -z "$TICKET" ]]; then
+  printf "mising regex pattern 'LP[A-Z]-[0-9]' in the current git branch\n" >&2
+  printf "missing argment: TICKET\nusage: cliferay jira TICKET\n" >&2\
+
+  exit 1
+fi
+
+echo "opening 🌏 https://liferay.atlassian.net/browse/$TICKET"
+open "https://liferay.atlassian.net/browse/$TICKET"
