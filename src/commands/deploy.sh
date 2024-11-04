caa7543955e9bde6b0e71b1a6d883de2fbcedf28
Alejandro Tardín <alejandro.tardin@liferay.com>
2024-11-04 12:06:09 +0100

    Supporting deploy of gradle-plugins

--- src/commands/deploy.sh
+++ src/commands/deploy.sh
@@ -8,6 +8,14 @@ elif [[ "$PWD" = *test-util ]]; then
     cliferay gw -a depl
     JAR=$( echo "$(realpath $PWD --relative-to $(cliferay folder))" | sed 's#modules/apps/[^/]*/\([^/]*\)-test-util#\1#' | sed 's/-/./g' | sed 's/\(.*\)/com.liferay.\1.test.util.jar/')
     cp "$(cliferay folder)/../bundles/osgi/test/$JAR" "$(cliferay folder)/../bundles/osgi/modules/"
+elif [[ "$PWD" = *gradle-plugins* ]]; then
+    cliferay gw installCache updateFileVersions
+    git add $(cliferay folder) && git commit -m "DELETE ME"
+    cd $(cliferay folder)/modules/sdk/gradle-plugins && gw installCache updateFileVersions
+    git add $(cliferay folder) && git commit --amend --no-edit
+    cd $(cliferay folder)/modules/sdk/gradle-plugins-defaults && gw installCache updateFileVersions
+    git add $(cliferay folder) && git commit --amend --no-edit
+    cd $(cliferay folder) && cliferay ant setup-sdk
 elif ! [[ "$PWD" = */*-test ]]; then
     cliferay gw -a depl
 fi
