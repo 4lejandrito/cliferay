051d41655a5f5b626a09aed98e36b6976c79059f
Alejandro Tardín <alejandro@tardin.com>
2024-12-10 01:45:10 +0100

    Colorizing
    ./bashly add colors

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
 sudo docker run --rm -it --user $(id -u):$(id -g) --volume "$PWD:/app" dannyben/bashly:1.2.5 generate --upgrade
-cat README.md | perl -0777 -pe "s/\`\`\`(.*?)\`\`\`/\`\`\`\n$(./cliferay --help)\n\`\`\`/s" > README.md
+cat README.md | perl -0777 -pe "s/\`\`\`(.*?)\`\`\`/\`\`\`\n$(NO_COLOR=1 ./cliferay --help)\n\`\`\`/s" > README.md
