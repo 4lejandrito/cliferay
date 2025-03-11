a384164ddec144a9384255aff158f07e2ab6d3fb
Alejandro Tardín <alejandro.tardin@liferay.com>
2025-03-11 11:02:15 +0100

    Saving URLs as attachments

--- src/commands/todo.sh
+++ src/commands/todo.sh
@@ -1,8 +1,21 @@
+TITLE="${other_args[*]}"
+URLS=($(grep -oE 'https?://[^ ]+' <<<"$TITLE" || true))
+CLEAN_TITLE=$(echo "$TITLE" | sed -E 's_https?://[^ ]+__g' | xargs)
+
 RESPONSE=$(curl -s -X POST "https://api.trello.com/1/cards" \
-    -d "name=${other_args[*]}" \
+    -d "name=$CLEAN_TITLE" \
     -d "idList=$TRELLO_LIST_ID" \
     -d "key=$TRELLO_API_KEY" \
     -d "token=$TRELLO_TOKEN")
+
+CARD_ID=$(echo "$RESPONSE" | jq -r '.id // empty')
+
+for URL in "${URLS[@]}"; do
+    curl -s -X POST "https://api.trello.com/1/cards/$CARD_ID/attachments" \
+        -d "url=$URL" \
+        -d "key=$TRELLO_API_KEY" \
+        -d "token=$TRELLO_TOKEN" >/dev/null
+done
 
 if [[ $RESPONSE == *'"id"'* ]]; then
     echo "$RESPONSE" | jq -r '.shortUrl'
