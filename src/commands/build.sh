442cbaff3e9c8ebfa8a152b31e4207ce81ad6c7f
Alejandro Tardín <alejandro.tardin@liferay.com>
2025-06-24 14:08:53 +0200

    Building test modules too
    https://github.com/brianchandotcom/liferay-portal/pull/162965

--- src/commands/build.sh
+++ src/commands/build.sh
@@ -1,5 +1,8 @@
 cd $(cliferay folder)
 cliferay ant setup-profile-dxp
 cliferay ant all
+(cd modules/util/portal-tools-rest-builder-test-api && cliferay deploy)
+(cd modules/util/portal-tools-rest-builder-test-client && cliferay deploy)
+(cd modules/util/portal-tools-rest-builder-test-impl && cliferay deploy)
 cd modules/test/playwright
 npm install
