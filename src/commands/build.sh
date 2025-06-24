276d088e3d60db7c58958a0a812c33b4cbb4958c
Alejandro Tardín <alejandro.tardin@liferay.com>
2025-06-24 14:09:55 +0200

    Oneliner

--- src/commands/build.sh
+++ src/commands/build.sh
@@ -4,5 +4,4 @@ cliferay ant all
 (cd modules/util/portal-tools-rest-builder-test-api && cliferay deploy)
 (cd modules/util/portal-tools-rest-builder-test-client && cliferay deploy)
 (cd modules/util/portal-tools-rest-builder-test-impl && cliferay deploy)
-cd modules/test/playwright
-npm install
+(cd modules/test/playwright && npm install)
