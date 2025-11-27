92ca9128eb5cad617c28dfb9827760bcea436a90
Alejandro Tardín <alejandro.tardin@liferay.com>
2025-11-27 09:46:38 +0100

    Fixing cd* aliases as the paths were being hardcoded on first evaluation

--- src/commands/aliases.sh
+++ src/commands/aliases.sh
@@ -1,4 +1,4 @@
 echo 'alias gw="cliferay gw"'
-echo 'alias cdc="cd $(cliferay source-folder)"'
-echo 'alias cdl="cd $(cliferay home)"'
+echo 'alias cdc="cd \$(cliferay source-folder)"'
+echo 'alias cdl="cd \$(cliferay home)"'
 echo 'alias c="cliferay"'
