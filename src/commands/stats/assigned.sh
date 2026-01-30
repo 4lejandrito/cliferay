a6aecb939c6b8b660aa27587d00912ce1b142ba1
Alejandro Tardín <alejandro.tardin@liferay.com>
2026-01-30 13:57:18 +0100

    Better query. Thanks Nora.

--- src/commands/stats/assigned.sh
+++ src/commands/stats/assigned.sh
@@ -1,6 +1,6 @@
 get_current_period
 
-JQL="assignee was $(cliferay stats users jira ${args["user"]}) during (${since%%T*}, ${until%%T*}) AND project IS NOT EMPTY AND updated >= ${since%%T*}"
+JQL="assignee was $(cliferay stats users jira ${args["user"]}) during (${since%%T*}, ${until%%T*}) AND project IS NOT EMPTY AND issue in updatedBy($(cliferay stats users jira ${args["user"]}), "${since%%T*}", "${until%%T*}") ORDER BY updated DESC"
 
 curl -s -u "$JIRA_API_USER:$JIRA_API_TOKEN" \
     -H "Accept: application/json" \
