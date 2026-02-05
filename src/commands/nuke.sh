b62ad3327f3273640ac7dc19a5a9415ce129aeb4
Alejandro Tardín <alejandro.tardin@liferay.com>
2026-02-05 15:23:39 +0100

    For you @ccorreagg

--- src/commands/nuke.sh
+++ src/commands/nuke.sh
@@ -1,3 +1,3 @@
 cd $(cliferay home)
-rm -rf ../bundles/elasticsearch7 ../bundles/data ../bundles/osgi/war ../bundles/osgi/state ../bundles2 $(cliferay tomcat-folder)/work/Catalina
+rm -rf ../bundles/elasticsearch* ../bundles/data ../bundles/osgi/war ../bundles/osgi/state ../bundles2 $(cliferay tomcat-folder)/work/Catalina
 echo "drop database IF EXISTS $(cliferay db-name); create database $(cliferay db-name) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin" | mysql -uroot -proot
