b362b2d82322887f22b5360f91d50fd5a8b17d78
Alejandro Tardín <alejandro.tardin@liferay.com>
2026-04-09 14:26:34 +0200

    No longer needed

--- src/commands/build.sh
+++ src/commands/build.sh
@@ -4,4 +4,3 @@ cliferay ant all
 (cd modules/util/portal-tools-rest-builder-test-api && cliferay deploy)
 (cd modules/util/portal-tools-rest-builder-test-client && cliferay deploy)
 (cd modules/util/portal-tools-rest-builder-test-impl && cliferay deploy)
-(cd modules/test/playwright && npm install)
