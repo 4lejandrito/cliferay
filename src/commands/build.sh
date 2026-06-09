3869b9253ca14b29439eb0de7e65a4f18461c7e6
Alejandro Tardín <alejandro.tardin@liferay.com>
2026-06-09 08:34:32 +0200

    No longer needed
    https://liferay.atlassian.net/browse/LPD-88834

--- src/commands/build.sh
+++ src/commands/build.sh
@@ -1,5 +1,4 @@
 cd $(cliferay home)
-cliferay ant setup-profile-dxp
 cliferay ant all
 (cd modules/util/portal-tools-rest-builder-test-api && cliferay deploy)
 (cd modules/util/portal-tools-rest-builder-test-client && cliferay deploy)
